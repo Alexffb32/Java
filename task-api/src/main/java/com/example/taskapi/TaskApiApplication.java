9440fcfb650eefc05a5adfe843e24b408540b500
Alexffb <110848750+Alexffb32@users.noreply.github.com>
2023-12-04 16:32:24 +0000

    Add files via upload

---
+++ task-api/src/main/java/com/example/taskapi/TaskApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskApiApplication.class, args);
+	}
+
+}
