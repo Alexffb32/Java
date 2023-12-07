28bec24c2c969be74fb3114da1be9000f476c5be
Alexffb <110848750+Alexffb32@users.noreply.github.com>
2023-12-07 14:48:25 +0000

    Delete UFCD0816_JAVA_2023-main (2)/UFCD0816_JAVA_2023-main directory

--- UFCD0816_JAVA_2023-main (2)/UFCD0816_JAVA_2023-main/README.md
+++
@@ -1,2 +0,0 @@
-# Documentação da GenerativeAI Tool
-https://chat.openai.com/share/11ec4836-d9b2-4b04-b85c-35d107f8f2fa
--- UFCD0816_JAVA_2023-main (2)/UFCD0816_JAVA_2023-main/src/main/java/com/example/demo/repositories/taskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.models.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface taskRepository extends JpaRepository<Task, Long> {
-    // Métodos personalizados, se necessário
-}
